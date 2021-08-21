76c860d4dd993939d0de3069726eaed94d748c2b
Alfonso Cerv <pedrocerv1@hotmail.com>
2021-08-21 12:57:59 -0500

    jsonwebtoken auth.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,14 +1,21 @@
-const jwt = require('jsonwebtoken');
+onst jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  authMiddleware: function ({req}) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.query.token || req.headers.authorization || req.body.token;
+    console.log('\n this is the request \n',req.body)
+    console.log('\n This is the Token \n',token);
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
@@ -16,7 +23,7 @@ module.exports = {
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req; //res.status(400).json({ message: 'You have no token!' }); (original starter code)
     }
 
     // verify token and get user data out of it
@@ -25,15 +32,11 @@ module.exports = {
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+
+      return console.log('this is a return!!'); //return res.status(400).json({ message: 'invalid token!' });
     }
 
     // send to next endpoint
-    next();
-  },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    return req;
   },
 };
